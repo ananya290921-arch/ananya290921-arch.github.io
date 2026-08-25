bc2b51bbab391f038fe02939f502136ed556daf4
ananya290921-arch <ananya290921@gmail.com>
2026-08-25 09:22:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventlistener('DOMContentLoaded',function () {
+ document.getElementByld('year').innerHTML=new Date().getFullyear();  
   console.log('Portfolioของ Ananya พร้อมแล้ว!';
 });
